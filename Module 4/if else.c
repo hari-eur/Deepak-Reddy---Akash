10575df3e76ad7b3ecf75a9a18bb5e1fad7efed9
N Deepak Reddy <98102633+deepakreddy729@users.noreply.github.com>
2023-02-22 10:50:44 +0530

    Update if else.c

--- Module 4/if else.c
+++ Module 4/if else.c
@@ -1,1 +1,27 @@
 #Write a program to print biggest of 3 numbers using if else 
+/*code*/
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter the three numbers :");
+    scanf("%d %d %d",&a,&b,&c);
+    if((a>b) && (a>c))
+    {
+        printf(" %d is biggest number",a);
+    }
+    else if((b>c) && (b>a))
+    {
+        printf(" %d is biggest number",b);
+    }
+    else
+    {
+        printf(" %d is biggest number",c);
+    }
+    return 0;
+}
+
+/*output
+Enter the three numbers :5 7 2
+7 is biggest number
+*/
